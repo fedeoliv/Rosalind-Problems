e3651c68bb367270d60ad7fcb72197cab60df856
Fernando Ferreira <bse.fernando@gmail.com>
2013-10-17 10:14:19 -0300

    Create gc_content.py

---
+++ gc_content.py
@@ -0,0 +1,23 @@
+#http://rosalind.info/problems/gc/
+
+dnaFile = open('gc_dna_content.txt', 'r')
+dna_fasta = dnaFile.readlines()
+dnaFile.close()
+
+gc_content = {}
+info_dna = ''
+
+for elements in dna_fasta:
+    if elements[0] == '>':
+        info_dna = elements[1:].rstrip()
+        gc_content[info_dna] = ''
+    else:
+        gc_content[info_dna] += elements.rstrip()
+
+for id_content, content in gc_content.iteritems():
+    gc_content[id_content] = (float(content.count('G') + content.count('C')) / len(content)) * 100
+
+(max_id, max_gc) = max(list(gc_content.iteritems()), key = lambda item:item[1])
+
+print max_id
+print("%.6f" % max_gc)

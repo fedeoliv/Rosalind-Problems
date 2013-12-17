ce84602fbc1dcc416187dbee93dddd8439a54fee
Fernando Ferreira <bse.fernando@gmail.com>
2013-12-17 00:14:28 -0200

    Global Alignment with Scoring Matrix and Affine Gap Penalty

---
+++ gaff.rb
@@ -0,0 +1,46 @@
+# http://rosalind.info/problems/gaff/
+
+PARAM = [11,1]
+require 'tempfile'
+NEEDLE = 'needle'
+seqs = []
+seq = ''
+gets
+
+while gets
+  if $_[0..0]=='>'
+      seqs<<seq
+      seq=''
+  else
+      seq+=$_.chomp
+  end
+end
+
+seqs << seq
+
+Tempfile.open('fasta1','.'){|fasta1|
+Tempfile.open('fasta2','.'){|fasta2|
+        fasta1.puts ">1\n#{seqs[0]}"
+        fasta1.flush
+        fasta2.puts ">2\n#{seqs[1]}"
+        fasta2.flush
+        io = open(%Q(| "#{NEEDLE}" -gapopen #{PARAM[0]} -gapextend #{PARAM[1]} -endweight -endopen #{PARAM[0]} -endextend #{PARAM[1]} -outfile stdout "#{File.basename fasta1.path}" "#{File.basename fasta2.path}"))
+        until (l=io.gets)=~/Length/ do; end
+        l=~/(\d+)/
+        len=$1.to_i
+        until (l=io.gets)=~/Score/ do; end
+        l=~/(-?\d+)/
+        puts $1
+if PARAM[1]==1 #lol
+        seqs=[]
+        until io.eof?
+                l=io.gets
+                if l=~/([-A-Z]+)/
+                        seqs<<$1
+                end
+        end
+        seqs.each_slice(2).to_a.transpose.each{|e|
+                puts e.join[0,len]
+        }
+end
+}}
